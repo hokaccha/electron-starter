5d4c2a5a2d9a6d8c5ea85da56abd66e00e3aa055
Kazuhito Hokamura <k.hokamura@gmail.com>
2018-01-02 16:40:57 +0900

    Add Button component

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import Button from './components/Button';
 
 import './app.css';
 
 class App extends React.Component<any, any> {
   render() {
-    return <div className="App">Hello Electron!</div>;
+    return <div className="App">
+      <h1>Hello Electron!</h1>
+      <Button onClick={e => this.handleClickButton()}>Click me!</Button>
+    </div>;
+  }
+
+  handleClickButton() {
+    console.log('clicked!');
   }
 }
 
---
+++ src/renderer/components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+interface ButtonProps {
+  onClick: (event: React.MouseEvent<HTMLSpanElement>) => void;
+}
+
+export default class Button extends React.Component<ButtonProps> {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e: React.MouseEvent<HTMLSpanElement>) {
+    if (this.props.onClick) {
+      this.props.onClick(e);
+    }
+  }
+
+  render() {
+    return <span className="Button" onClick={this.handleClick}>{this.props.children}</span>;
+  }
+}
