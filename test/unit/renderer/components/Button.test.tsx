6a0517d0a37c609db5333af9a60c13166f53ae34
Kazuhito Hokamura <k.hokamura@gmail.com>
2018-02-12 12:16:22 +0900

    Improve scripts

--- test/unit/renderer/components/Button.test.tsx
+++ test/unit/renderer/components/Button.test.tsx
@@ -3,7 +3,7 @@ import sinon from "sinon";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import assert from "assert";
-import Button from "./Button";
+import Button from "../../../../src/renderer/components/Button/Button";
 
 Enzyme.configure({ adapter: new Adapter() });
 
